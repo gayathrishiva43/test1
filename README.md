8e54e0258ecb6ea4eac8fd0a82affa4998da7e26
john <bibin@gmail.com>
2024-02-20 21:28:26 +0530

    empty the branch

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# test1
+seperated branch created
--- test/itemModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-const itemSchema = new mongoose.Schema({})
-
-module.exports = mongoose.model('Item', itemSchema)
